c2e38f99d7f6907553d28b974295971ba21170d6
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2026-01-14 23:37:08 +0100

    treesitter: fix because of breaking changes

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,18 +1,11 @@
-local configs = require('nvim-treesitter.configs')
+local treesitter = require('nvim-treesitter')
 vim.g.skip_ts_context_comment_string_module = true
 
 ---@diagnostic disable-next-line: missing-fields
-configs.setup {
-    highlight = {
-        enable = true,
-    },
-    indent = {
-        enable = true,
-    },
-}
+treesitter.setup {}
 
 require('ts_context_commentstring').setup()
 
 -- Tree-sitter based folding
 -- vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
