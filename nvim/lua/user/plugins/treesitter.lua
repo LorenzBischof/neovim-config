4159780413e118a9bc3f0aa3e842aaef58eadbbc
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2026-04-03 13:42:52 +0200

    treesitter: fix injections

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -6,6 +6,19 @@ treesitter.setup {}
 
 require('ts_context_commentstring').setup()
 
+local ts_start_group = vim.api.nvim_create_augroup('user-treesitter-start', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FileType' }, {
+  group = ts_start_group,
+  callback = function(args)
+    if vim.bo[args.buf].buftype ~= '' then
+      return
+    end
+
+    pcall(vim.treesitter.start, args.buf)
+  end,
+})
+
 -- Tree-sitter based folding
 -- vim.opt.foldmethod = 'expr'
 -- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
